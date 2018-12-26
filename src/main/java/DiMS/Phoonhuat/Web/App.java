63c9e8431ef825f978fa7d199ebad1766577b275
quangnmh <quanghuynh1403@gmail.com>
2018-12-26 14:14:57 +0700

    first commit

---
+++ src/main/java/DiMS/Phoonhuat/Web/App.java
@@ -0,0 +1,13 @@
+package DiMS.Phoonhuat.Web;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
